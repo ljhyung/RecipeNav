4c7166b5ee5c92274540d67270d804eb1d8eed09
손광진 <start_so@naver.com>
2022-09-22 00:35:13 +0900

    [FIX] 로그인 과정 수정

--- FE/recipe-nav/src/pages/Page1.js
+++ FE/recipe-nav/src/pages/Page1.js
@@ -1,12 +1,28 @@
-import style from './Page1.module.css';
-import 'antd/dist/antd.css';
-import Home from './Home/Home';
+import style from "./Page1.module.css";
+import "antd/dist/antd.css";
+import Home from "./Home/Home";
+import { nanoid } from "nanoid";
 
 const Page1 = () => {
+  console.log("Page1 로딩");
+
+  const clientId = process.env.REACT_APP_NAVER_CLIENT_ID;
+  const authUrl = process.env.REACT_APP_NAVER_AUTH_URL;
+  const redirectUrl = process.env.REACT_APP_NAVER_REDIRECT_URL;
+
+  const state = nanoid();
+
+  let requestUrl = authUrl;
+  requestUrl += "?response_type=code";
+  requestUrl += `&client_id=${clientId}`;
+  requestUrl += `&state=${state}`;
+  requestUrl += `&redirect_uri=${redirectUrl}`;
+
   return (
     <>
       Hellow
       <Home />
+      <a href={requestUrl}>네이버 로그인</a>
     </>
   );
 };
--- FE/recipe-nav/src/pages/oauth/NaverOauth.js
+++ FE/recipe-nav/src/pages/oauth/NaverOauth.js
@@ -5,16 +5,29 @@ import axiosClient from "../../api";
 const NaverOauth = () => {
   const location = useLocation();
   const navigate = useNavigate();
-  const code = location.search.split("=")[1];
+  const code = location.search.split("=")[1].split("&")[0];
   const state = location.search.split("=")[2];
 
   console.log(code);
   console.log(state);
   useEffect(() => {
     axiosClient
-      .get(`/oauth/authentication/naver?code=${code}&state=${state}`)
+      .post(`/oauth/authentication/naver`, {
+        code,
+        state,
+      })
       .then((response) => {
+        console.log("응답값");
         console.log(response);
+
+        axiosClient
+          .get("/my-infos")
+          .then((res) => {
+            console.log(res);
+          })
+          .catch((error) => {
+            console.log(error);
+          });
       })
       .catch((error) => {
         console.log(error);
