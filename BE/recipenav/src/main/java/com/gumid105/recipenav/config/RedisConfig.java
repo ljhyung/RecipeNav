d7777212c84ad71bc1ddb024a1f98140feed1cc1
손광진 <start_so@naver.com>
2022-09-27 16:58:14 +0900

    [ADD] BE , 이미지 캐싱을 위한 레디스 적용

---
+++ BE/recipenav/src/main/java/com/gumid105/recipenav/config/RedisConfig.java
@@ -0,0 +1,24 @@
+package com.gumid105.recipenav.config;
+
+import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
+import org.springframework.data.redis.core.RedisTemplate;
+
+@EnableCaching
+@Configuration
+public class RedisConfig {
+
+    public static String IMAGE = "proxy-img:";
+    @Bean
+    public LettuceConnectionFactory redisConnectionFactory() {
+        return new LettuceConnectionFactory("j7d105.p.ssafy.io", 6379);
+    }
+    @Bean
+    public RedisTemplate<?, ?> redisTemplate() {
+        RedisTemplate<String, byte[]> redisTemplate = new RedisTemplate<>();
+        redisTemplate.setConnectionFactory(redisConnectionFactory());
+        return redisTemplate;
+    }
+}
