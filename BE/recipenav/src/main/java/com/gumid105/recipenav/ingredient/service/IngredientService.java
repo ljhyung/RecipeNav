1dae2c86acc840be36fec1b150f2563f44207121
LJH <ljjuni1114@gmail.com>
2022-09-28 17:24:34 +0900

    [BE] - ADD : 재료 조회 추가

--- BE/recipenav/src/main/java/com/gumid105/recipenav/ingredient/service/IngredientService.java
+++ BE/recipenav/src/main/java/com/gumid105/recipenav/ingredient/service/IngredientService.java
@@ -2,6 +2,7 @@ package com.gumid105.recipenav.ingredient.service;
 
 import com.gumid105.recipenav.ingredient.domain.Ingredient;
 import com.gumid105.recipenav.ingredient.domain.IngredientPriceLog;
+import com.gumid105.recipenav.ingredient.dto.ChangeRate;
 import com.gumid105.recipenav.ingredient.dto.IngredientDto;
 import com.gumid105.recipenav.ingredient.dto.IngredientPriceLogDto;
 import com.gumid105.recipenav.ingredient.dto.IngredientPriceLogSeparateDto;
@@ -17,8 +18,10 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import java.time.LocalDate;
 import java.util.List;
 import java.util.Optional;
+import java.util.PriorityQueue;
 
 @Service
 @Transactional
@@ -28,11 +31,16 @@ public class IngredientService {
     private final RecipeRepository recipeRepository;
     private final IngredientPriceLogRepository ingredientPriceLogRepository;
 
-    public Page<Ingredient> getAllIngredient(Integer page, Integer size){
+    public Page<Ingredient> getAllIngredientByPage(Integer page, Integer size){
         PageRequest pageRequest = PageRequest.of(page,size);
         Page<Ingredient> ingredientPage = ingredientRepository.findAll(pageRequest);
 //        List<Ingredient> ingredientList = ingredientRepository.findAll(pageRequest).getContent();
         return ingredientPage;
+    }
+
+    public List<IngredientDto> getAllIngredient(){
+        List<Ingredient> ingredientList = ingredientRepository.findAll();
+        return IngredientDto.ofList(ingredientList);
     }
 
     public IngredientDto getIngredientDetail(Long seq){
@@ -67,6 +75,32 @@ public class IngredientService {
         List<IngredientPriceLog> ingredientPriceLogList = ingredientPriceLogRepository.findIngredientPriceLogsByIngredient_IngSeqOrderByIngDate(ingredientSeq);
         return IngredientPriceLogSeparateDto.ofList(ingredientPriceLogList);
     }
+
+//    public Double getIngredientByTopGainers(){
+//        PriorityQueue<ChangeRate> priorityQueue = new PriorityQueue<>(10);
+//        for(Long i=0l;i<100;i++){
+//
+//            List<IngredientPriceLog> ingredientPriceLogList = ingredientPriceLogRepository.findIngredientPriceLogsByIngredient_IngSeqOrderByIngDate(i);
+//            Integer logLength = ingredientPriceLogList.size();
+//            System.out.println("진입"+i+", "+"길이"+logLength);
+//            if (logLength<10) continue;
+//            if (ingredientPriceLogList.get(logLength-10).getIngDate().isBefore(LocalDate.of(2022, 7, 29))) continue;
+//            System.out.println("날짜 만족");
+//            Double result = (ingredientPriceLogList.get(logLength-1).getIngAvgCost()-ingredientPriceLogList.get(logLength-11).getIngAvgCost())*100/ingredientPriceLogList.get(logLength-1).getIngAvgCost();
+//            priorityQueue.add(new ChangeRate(result,ingredientPriceLogList.get(0).getIngredient()));
+//        }
+////        List<IngredientPriceLog> ingredientPriceLogList = ingredientPriceLogRepository.findIngredientPriceLogsByIngredient_IngSeqOrderByIngDate(ingredientSeq);
+////        Double todayResult = ingredientPriceLogList.get(ingredientPriceLogList.size()-1).getIngAvgCost();
+////        Double pastResult = ingredientPriceLogList.get(ingredientPriceLogList.size()-11).getIngAvgCost();
+////        Double result = (todayResult - pastResult)*100/todayResult;
+//        while(!priorityQueue.isEmpty()){
+//            ChangeRate temp = priorityQueue.poll();
+//            System.out.println(temp.getRate()+""+ temp.getIngredient().getIngSeq());
+//        }
+//        Double result=1d;
+//        return result;
+//    }
+
 
 
 }
