3e2715976b34efa3395b9417e15d6530381c8318
손광진 <start_so@naver.com>
2022-09-29 19:32:29 +0900

    [ADD] 레디스 클러스터 설정

--- BE/recipenav/src/main/java/com/gumid105/recipenav/config/RedisConfig.java
+++ BE/recipenav/src/main/java/com/gumid105/recipenav/config/RedisConfig.java
@@ -1,8 +1,11 @@
 package com.gumid105.recipenav.config;
 
+import io.lettuce.core.ReadFrom;
 import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.connection.RedisClusterConfiguration;
+import org.springframework.data.redis.connection.lettuce.LettuceClientConfiguration;
 import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
 
@@ -15,6 +18,17 @@ public class RedisConfig {
     public static String TOPLOSER = "top-loser";
     @Bean
     public LettuceConnectionFactory redisConnectionFactory() {
+        LettuceClientConfiguration clientConfiguration = LettuceClientConfiguration.builder().
+                readFrom(ReadFrom.REPLICA_PREFERRED).build();
+
+        RedisClusterConfiguration redisClusterConfiguration = new RedisClusterConfiguration();
+        redisClusterConfiguration.clusterNode("j7d105.p.ssafy.io",6379);
+        redisClusterConfiguration.clusterNode("j7d105.p.ssafy.io",7000);
+        redisClusterConfiguration.clusterNode("j7d105.p.ssafy.io",7001);
+        redisClusterConfiguration.clusterNode("j7d105.p.ssafy.io",7002);
+        redisClusterConfiguration.clusterNode("j7d105.p.ssafy.io",7003);
+        redisClusterConfiguration.clusterNode("j7d105.p.ssafy.io",7004);
+
         return new LettuceConnectionFactory("j7d105.p.ssafy.io", 6379);
     }
     @Bean
