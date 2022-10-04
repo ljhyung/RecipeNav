a191dafd4147c9db3cdcc6fcfa4a00ea795f799a
bok <vvs9439@naver.com>
2022-10-04 14:17:26 +0900

    [FE] 식자재 폰트

--- FE/recipe-nav/src/components/ingredient/IngredientCardComponent.js
+++ FE/recipe-nav/src/components/ingredient/IngredientCardComponent.js
@@ -1,45 +1,63 @@
 import React from "react";
 import style from "./IngredientCardComponent.module.css";
-import { Image } from "antd";
-import { proxyImageURL } from "../../api";
+import {Image} from "antd";
+import {proxyImageURL} from "../../api";
 import Graph from "./Graph";
+import { CaretDownOutlined, CaretUpOutlined } from "@ant-design/icons";
 const IngredientCardComponent = (props) => {
-  const ingredient = props.ingredient;
-  let imageUrl = ingredient.ingImg;
-  let logList = ingredient.ingredientPriceLogList.slice(-10);
-  if (imageUrl.startsWith("https")) {
-  } else {
-    imageUrl = proxyImageURL + imageUrl;
-  }
-  return (
-    <>
-      <div className={style["card-frame"]}>
-        <div className={style.head}>
-          <Image src={imageUrl} style={{ borderRadius: "5px" }} height={200} />
-          <div className={style.graph}>
-            <Graph data={logList} />
-          </div>
+    const ingredient = props.ingredient;
+    let imageUrl = ingredient.ingImg;
+    let logList = ingredient
+        .ingredientPriceLogList
+        .slice(-10);
+    if (imageUrl.startsWith("https")) {} else {
+        imageUrl = proxyImageURL + imageUrl;
+    }
+    console.log(ingredient)
+    return (
+        <> < div className = {
+            style["card-frame"]
+        } > <div className={style.head}>
+            <Image
+                src={imageUrl}
+                style={{
+                    borderRadius: "5px"
+                }}
+                height={200}/>
+            <div className={style.graph}>
+                <Graph data={logList}/>
+            </div>
         </div>
         <div className={style.body}>
-          <div
-            className={style["ingredient-card-title"]}
-            onClick={() => props.ingredientClickHandle(ingredient.ingSeq)}
-          >
-            {ingredient.ingName}
-          </div>
-          <div className={style["line"]} />
+            <div
+                className={style["ingredient-card-title"]}
+                onClick={() => props.ingredientClickHandle(ingredient.ingSeq)}>
+                {ingredient.ingName}
+            </div>
+            <div className={style["line"]}/>
 
-          <div className={style.boxbox}>
-            <div>{ingredient.ingPriceRate}</div>
-            <div>{ingredient.ingredientPrice.ingMinCost}</div>
-            <div>{ingredient.ingredientPrice.ingAvgCost}</div>
-            <div>{ingredient.ingredientPrice.ingMaxCost}</div>
-          </div>
+            <div className={style.boxbox}>
+
+                {
+                    ingredient.ingPriceRate != null
+                        ? ingredient.ingPriceRate >= 0
+                            ? <div style={{color: 'red'}}><CaretUpOutlined/>{(ingredient.ingPriceRate).toFixed(2)}
+                                    %
+                                </div>
+                            : <div style={{color: 'blue'}}><CaretDownOutlined/>{(ingredient.ingPriceRate).toFixed(2)}
+                                    %
+                                </div>
+                        : <div>-%</div>
+                }
+                <div>최저가: {ingredient.ingredientPrice.ingMinCost}원</div>
+                <div>평균가: {ingredient.ingredientPrice.ingAvgCost}원</div>
+                <div>최대가: {ingredient.ingredientPrice.ingMaxCost}원</div>
+            </div>
         </div>
         <div className={style.foot}></div>
-      </div>
-    </>
-  );
+    </div>
+</>
+    );
 };
 
 export default IngredientCardComponent;
