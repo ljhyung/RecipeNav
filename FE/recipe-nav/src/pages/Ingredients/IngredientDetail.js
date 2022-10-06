e3253a872e2582d00e858e3e3d1097c9e17649f0
bok <vvs9439@naver.com>
2022-10-06 16:18:43 +0900

    [FE]배너수정

--- FE/recipe-nav/src/pages/Ingredients/IngredientDetail.js
+++ FE/recipe-nav/src/pages/Ingredients/IngredientDetail.js
@@ -1,71 +1,109 @@
 import React from "react";
-import { useNavigate, useParams } from "react-router-dom";
-import { useDispatch, useSelector } from "react-redux";
+import {useNavigate, useParams} from "react-router-dom";
+import {useDispatch, useSelector} from "react-redux";
 import style from "./IngredientDetail.module.css";
-import { Collapse, Image } from "antd";
-import { proxyImageURL } from "../../api";
-import { RollbackOutlined } from "@ant-design/icons";
+import {Collapse, Image, Table} from "antd";
+
+import {proxyImageURL} from "../../api";
+import {RollbackOutlined} from "@ant-design/icons";
 import IngredientGraphComponent from "../../components/ingredient/IngredientGraphComponent";
 
 import IngredientScrapButtonComponent from "../../components/ingredient/IngredientScrapButtonComponent";
 import RecommendedRecipeComponent from "../../components/ingredient/RecommendedRecipeComponent";
 
 const comparator = function (a, b) {
-  return a.ingOrder - b.ingOrder;
+  return a.ingDate - b.ingDate;
 };
 
+const columns = [
+  {
+    title: '날짜',
+    dataIndex: 'ingDate',
+    key: 'ingDate',
+  },
+  {
+    title: '최대가',
+    dataIndex: 'ingMaxCost',
+    key: 'ingMaxCost',
+  },
+  {
+    title: '최저가',
+    dataIndex: 'ingMinCost',
+    key: 'ingMinCost',
+  },
+  {
+    title: '평균가',
+    dataIndex: 'ingAvgCost',
+    key: 'ingAvgCost',
+  },
+]
+
+
+
 const IngredientDetail = (props) => {
-  const params = useParams();
-  const dispatch = useDispatch();
-  const navigate = useNavigate();
-  const accessToken = useSelector((state) => state.auth.accessToken);
+    const params = useParams();
+    const dispatch = useDispatch();
+    const navigate = useNavigate();
+    const accessToken = useSelector((state) => state.auth.accessToken);
 
-  console.log(params);
+    
+    console.log(params);
 
-  const selectedIngredient = useSelector(
-    (state) => state.ingredient.selectedIngredient,
-  );
+    const selectedIngredient = useSelector(
+        (state) => state.ingredient.selectedIngredient,
+    );
 
-  const backPageClickHandle = () => {
-    navigate(-1);
-  };
+    const backPageClickHandle = () => {
+        navigate(-1);
+    };
 
-  return (
-    <>
-      <div className={style["back-page"]} onClick={backPageClickHandle}>
-        <RollbackOutlined />
-      </div>
-      <div className={style["ingredient-detail-container"]}>
+    console.log(selectedIngredient);
+    
+    return (
+        <> < div className = {
+            style["back-page"]
+        }
+        onClick = {
+            backPageClickHandle
+        } > <RollbackOutlined/>
+    </div>
+    <div className={style["ingredient-detail-container"]}>
         <div className={style["recipe-detail-head"]}>
-          <h2 className={style["category-text"]}>
-            {selectedIngredient.ingName}
-          </h2>
-          <IngredientScrapButtonComponent
-            ingredientIng={selectedIngredient.ingSeq}
-          />
+            <h2 className={style["category-text"]}>
+                {selectedIngredient.ingName}
+            </h2>
+            <IngredientScrapButtonComponent ingredientIng={selectedIngredient.ingSeq}/>
         </div>
 
         <div className={style["ingredient-detail-meta"]}>
-          <div className={style["ingredient-detail-head-post"]}>
-            <Image
-              src={proxyImageURL + selectedIngredient.ingImg}
-              fluid="true"
-            />
-          </div>
-
-          <div className={style["ingredient-detail-desc"]}>
-            <div>평균가격</div>
-            <div>
-              <IngredientGraphComponent />
+            <div className={style["ingredient-detail-head-post"]}>
+                <Image src={proxyImageURL + selectedIngredient.ingImg} fluid="true"/>
             </div>
-          </div>
+
+            <div className={style["ingredient-detail-desc"]}>
+                <div>평균가격</div>
+                <div>
+                    <IngredientGraphComponent/>
+                </div>
+            </div>
         </div>
         <div className={style["footer"]}>
-          <RecommendedRecipeComponent />
+            <RecommendedRecipeComponent/>
         </div>
-      </div>
-    </>
-  );
+        <div className={style["recipe-detail-phase-container"]}>
+            <h2 className={style["phase-text-bar"]}>가격 전체 조회</h2>
+            <Collapse>
+                <Collapse.Panel>
+
+                    {/* //표만들기 시작 */}
+                    <Table columns={columns} dataSource={selectedIngredient.ingredientPriceLogList}/>
+
+                </Collapse.Panel>
+            </Collapse>
+        </div>
+    </div>
+</>
+    );
 };
 
 export default IngredientDetail;
